b49255ba2b6f643372fb68d7519a3190432805ca
Pradene <liam.pradene@gmail.com>
2026-01-19 17:53:53 +0100

    Fix: unmatched quote now check for all pairs of tokens, ex: (), [], {}

--- src/input.c
+++ src/input.c
@@ -1,26 +1,55 @@
 #include "strbuf.h"
-#include "vec.h"
 #include <readline/readline.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-static bool has_unfinished_quote(const char *s) {
+static bool has_unmatched_pair(const char *s) {
   bool in_single = false;
   bool in_double = false;
+
+  StringBuffer stack = {0};
 
   for (size_t i = 0; s[i]; ++i) {
     if (s[i] == '\\' && s[i + 1]) {
       ++i;
       continue;
     }
+
     if (s[i] == '\'' && !in_double) {
       in_single = !in_single;
+      continue;
     } else if (s[i] == '\"' && !in_single) {
       in_double = !in_double;
+      continue;
+    }
+
+    if (in_single || in_double) {
+      continue;
+    }
+
+    if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+      strbuf_push(&stack, s[i]);
+    } else if (s[i] == ')' || s[i] == ']' || s[i] == '}') {
+      if (stack.size == 0) {
+        strbuf_free(&stack);
+        return true;
+      }
+
+      char last = stack.data[stack.size - 1];
+      bool match = (s[i] == ')' && last == '(') ||
+                   (s[i] == ']' && last == '[') || (s[i] == '}' && last == '{');
+
+      if (!match) {
+        strbuf_free(&stack);
+        return true;
+      }
+      stack.size--;
     }
   }
 
-  return in_single || in_double;
+  bool result = in_single || in_double || stack.size > 0;
+  strbuf_free(&stack);
+  return result;
 }
 
 char *read_input() {
@@ -46,7 +75,7 @@ char *read_input() {
       continue;
     }
 
-    if (has_unfinished_quote(sb.data)) {
+    if (has_unmatched_pair(sb.data)) {
       strbuf_push(&sb, '\n');
       line = readline("> ");
       continue;
