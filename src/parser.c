8c57d669494cceead1c747e15d726dcc3fa2c188
Liam Pradene <liam.pradene@proton.me>
2026-01-20 11:56:40 +0100

    Fix: Parsing of groups () and {}

--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,6 @@
 #include "parser.h"
+#include "lexer.h"
+#include <stdio.h>
 
 static AstNode *parse_command(Tokens *tokens, size_t *i) {
   AstNode *node = (AstNode *)calloc(1, sizeof(AstNode));
@@ -28,25 +30,33 @@ static AstNode *parse_command(Tokens *tokens, size_t *i) {
   }
 }
 
+static AstNode *parse_group(Tokens *tokens, size_t *i);
 static AstNode *parse_pipeline(Tokens *tokens, size_t *i) {
-  AstNode *left = parse_command(tokens, i);
+  AstNode *left = parse_group(tokens, i);
   if (!left) {
     return NULL;
   }
 
-  while (*i < vec_size(tokens) && vec_at(tokens, *i).type == TOKEN_PIPE) {
+  while (*i < vec_size(tokens)) {
+    if (vec_at(tokens, *i).type != TOKEN_PIPE) {
+      break;
+    }
+
     ++(*i);
-    AstNode *right = parse_command(tokens, i);
+
+    AstNode *right = parse_group(tokens, i);
     if (!right) {
       ast_free(&left);
       return NULL;
     }
+
     AstNode *node = (AstNode *)calloc(1, sizeof(AstNode));
     if (!node) {
       ast_free(&left);
       ast_free(&right);
       return NULL;
     }
+
     node->type = NODE_PIPE;
     node->operator.right = right;
     node->operator.left = left;
@@ -119,10 +129,6 @@ static AstNode *parse_sequence(Tokens *tokens, size_t *i) {
     AstNode *right;
     if (*i < vec_size(tokens)) {
       right = parse_logical(tokens, i);
-      if (!right) {
-        ast_free(&left);
-        return NULL;
-      }
     } else {
       right = NULL;
     }
@@ -140,6 +146,36 @@ static AstNode *parse_sequence(Tokens *tokens, size_t *i) {
     left = node;
   }
   return left;
+}
+
+static AstNode *parse_group(Tokens *tokens, size_t *i) {
+  TokenType close;
+  AstNodeType type;
+
+  if (vec_at(tokens, *i).type == TOKEN_LPAREN) {
+    close = TOKEN_RPAREN;
+    type = NODE_PAREN;
+  } else if (vec_at(tokens, *i).type == TOKEN_LBRACE) {
+    close = TOKEN_RBRACE;
+    type = NODE_BRACE;
+  } else {
+    return parse_command(tokens, i);
+  }
+
+  ++(*i);
+  AstNode *inner = parse_sequence(tokens, i);
+  if (!inner) {
+    return NULL;
+  }
+  if (vec_at(tokens, *i).type != close) {
+    ast_free(&inner);
+    return NULL;
+  }
+  ++(*i);
+  AstNode *node = (AstNode *)calloc(1, sizeof(AstNode));
+  node->type = type;
+  node->group.inner = inner;
+  return node;
 }
 
 bool parse(Tokens *tokens, AstNode **root) {
